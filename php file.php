677d668805a359e0024bedcd79b5514b48301db1
Mahwishamin001 <145493774+Mahwishamin001@users.noreply.github.com>
2024-01-10 09:02:47 -0800

    Add files via upload

---
+++ php file.php
@@ -0,0 +1,303 @@
+// 2nd frame code
+<?php
+echo '<!DOCTYPE html>';
+echo '<html>';
+echo '<head>';
+echo '  <meta charset="utf-8" />';
+echo '  <link rel="icon" href="/favicon.ico" />';
+echo '  <meta name="viewport" content="width=device-width, initial-scale=1" />';
+echo '  <meta name="theme-color" content="#000000" />';
+echo '  <title>Frame 2</title>';
+echo '  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins%3A400%2C500%2C700"/>';
+echo '  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C500%2C700"/>';
+echo '  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Font+Awesome+5+Brands%3A400"/>';
+echo '  <link rel="stylesheet" href="./styles/frame-2.css"/>';
+echo '</head>';
+echo '<body>';
+echo '<div class="frame-2-rZr">';
+echo '  <div class="loginbutton-w5W">';
+echo '    <div class="rectangle-6-Snx">';
+echo '    </div>';
+echo '    <div class="log-in-Muv">Log in</div>';
+echo '  </div>';
+echo '  <div class="auto-group-vtpc-e8L">';
+echo '    <div class="signup-ang">';
+echo '      <div class="auto-group-bgqa-5De">';
+echo '        <div class="sign-up-bxg">Sign up</div>';
+echo '        <div class="closebutton-i1i">×</div>';
+echo '      </div>';
+echo '      <div class="social-login-MqN">';
+echo '        <div class="sign-up-with-google-5Fa">Sign up with Google</div>';
+echo '        <div class="item--B3i"></div>';
+echo '        <div class="rectangle-4-4NQ">';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="line-1-Lqi">';
+echo '      </div>';
+echo '      <div class="namefield-UBE">';
+echo '        <div class="name-zfN">Name</div>';
+echo '        <div class="auto-group-c3sr-LjE">';
+echo '          <div class="enter-your-full-name-6iQ">Enter Your Full Name</div>';
+echo '          <div class="rectangle-9-bQG">';
+echo '          </div>';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="emailfield-7Nc">';
+echo '        <div class="email-qJc">Email</div>';
+echo '        <div class="auto-group-ux5i-a1J">';
+echo '          <div class="enter-your-email-address-vax">Enter Your Email Address</div>';
+echo '          <div class="rectangle-10-2tt">';
+echo '          </div>';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="passwordfield-kZz">';
+echo '        <div class="password-HK2">Password</div>';
+echo '        <div class="auto-group-ueap-DCg">';
+echo '          <div class="at-least-8-characters-xvx">At least 8 characters</div>';
+echo '          <div class="rectangle-11-sHE">';
+echo '          </div>';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="agree-nQC">';
+echo '        <div class="rectangle-13-VZW">';
+echo '        </div>';
+echo '        <div class="i-agree-with-terms-and-privacy-23e">';
+echo '          <span class="i-agree-with-terms-and-privacy-23e-sub-0">I agree with </span>';
+echo '          <span class="i-agree-with-terms-and-privacy-23e-sub-1">Terms</span>';
+echo '          <span class="i-agree-with-terms-and-privacy-23e-sub-2"> and </span>';
+echo '          <span class="i-agree-with-terms-and-privacy-23e-sub-3">Privacy</span>';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="signupbutton-4HA">Sign up</div>';
+echo '      <div class="line-3-JhJ">';
+echo '      </div>';
+echo '      <div class="foot-qSL">';
+echo '        <div class="already-have-an-account-ADi">Already have an account?</div>';
+echo '        <div class="log-in-5rU">Log in</div>';
+echo '      </div>';
+echo '    </div>';
+echo '    <div class="login-M3J">';
+echo '      <div class="auto-group-gzzc-F8g">';
+echo '        <div class="log-in-Nj6">Log in</div>';
+echo '        <div class="closebutton-7Rn">×</div>';
+echo '      </div>';
+echo '      <div class="social-login-zEg">';
+echo '        <div class="item--tax"></div>';
+echo '        <div class="log-in-with-google-Cbe">Log in with Google</div>';
+echo '      </div>';
+echo '      <div class="auto-group-gsvw-UZA">';
+echo '        <div class="emailfield-PAL">';
+echo '          <div class="email-jEC">Email</div>';
+echo '          <div class="auto-group-anha-GE8">';
+echo '            <div class="enter-your-email-address-pFe">Enter Your Email Address</div>';
+echo '            <div class="rectangle-8-KiC">';
+echo '            </div>';
+echo '          </div>';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="passwordfield-chJ">';
+echo '        <div class="password-X3a">Password</div>';
+echo '        <div class="auto-group-kmoz-Rue">';
+echo '          <div class="enter-your-password-anY">Enter Your Password</div>';
+echo '          <div class="rectangle-7-toE">';
+echo '          </div>';
+echo '        </div>';
+echo '      </div>';
+echo '      <div class="remember-R2U">';
+echo '        <div class="rectangle-12-KtY">';
+echo '        </div>';
+echo '        <div class="remember-me-qrt">Remember Me</div>';
+echo '      </div>';
+echo '      <div class="loginbutton-NM2">Log in</div>';
+echo '      <div class="line-4-dXr">';
+echo '      </div>';
+echo '      <div class="foot-MTr">';
+echo '        <div class="dont-have-an-account-UoN">Don’t have an account?</div>';
+echo '        <div class="sign-up-CDa">Sign up</div>';
+echo '      </div>';
+echo '    </div>';
+echo '  </div>';
+echo '</div>';
+echo '</body>';
+echo '</html>';
+?>
+// first frame code 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <title>Mahwish's Jewellery </title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins%3A400%2C500%2C600"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Risque%3A400"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Slab%3A500%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400%2C500%2C600%2C700"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto+Serif%3A600"/>
+  <link rel="stylesheet" href="./styles/mahwishs-jewellery-.css"/>
+</head>
+<body>
+<div class="mahwishs-jewellery--ZSt">
+  <div class="auto-group-qb3r-kGU">
+    <p class="feedback-corner-qKr">Feedback Corner</p>
+    <div class="auto-group-zpig-Q1a">
+      <div class="group-5-GJg">
+        <div class="auto-group-7fcp-7KJ">
+          <p class="shanay-c1A">Shanay </p>
+          <p class="item--fGG">“</p>
+        </div>
+        <p class="the-customer-experience-was-exceptional-from-start-to-finish-the-website-is-user-friendly-the-checkout-process-was-smooth-and-the-jewellery-i-ordered-fit-perfectly-im-beyond-satisfied-UzQ">The customer experience was exceptional from start to finish. The website is user-friendly, the checkout process was smooth, and the jewellery I ordered fit perfectly. I&#39;m beyond satisfied!</p>
+      </div>
+      <div class="group-3-9jA">
+        <div class="auto-group-r1hn-4LL">
+          <p class="zahra-yiC">Zahra  </p>
+          <p class="item--5FS">“</p>
+        </div>
+        <p class="i-absolutely-love-the-quality-and-style-of-the-jewellery-i-purchased-from-this-website-customer-service-was-outstanding-and-i-received-my-order-quickly-highly-recommended-aCC">I absolutely love the quality and style of the jewellery I purchased from this website. customer service was outstanding, and I received my order quickly. Highly recommended!</p>
+      </div>
+      <div class="group-4-yEL">
+        <div class="auto-group-jyww-6Jx">
+          <p class="areeba-S7v">Areeba  </p>
+          <p class="item--ibE">“</p>
+        </div>
+        <p class="i-had-a-great-experience-shopping-on-this-website-the-jewellery-i-bought-are-fashionable-and-comfortable-highly-satisfied-puA">I had a great experience shopping on this website. The JEWELLERY I bought are fashionable and comfortable. Highly satisfied!</p>
+      </div>
+    </div>
+    <div class="auto-group-f4ur-FzU">
+      <img class="group-8-ArY" src="./assets/group-8.png"/>
+      <img class="group-9-W9i" src="./assets/group-9.png"/>
+    </div>
+    <div class="group-2-ELc">
+      <div class="auto-group-s8zp-Kcx">
+        <div class="frame-7-G2Q">
+        </div>
+        <div class="pexels-dima-valkov-6211617-1-nWY">
+        </div>
+      </div>
+      <div class="auto-group-nfux-Qnp">
+        <div class="group-1-XsS">
+          <p class="exclusive-offer-Gq2">Exclusive offer</p>
+          <p class="unlock-the-ultimate-style-upgrade-with-our-exclusive-offer-enjoy-savings-of-up-to-40-off-on-our-latest-new-arrivals-c8C">Unlock the ultimate style upgrade with our exclusive offer Enjoy savings of up to 40% off on our latest New Arrivals</p>
+          <div class="auto-group-sjzc-tbW">
+            <div class="group-22-pVA">
+              <p class="days-MV6">Days</p>
+              <p class="item-06-G6G">06</p>
+            </div>
+            <div class="group-21-XH6">
+              <p class="item-18-53i">18</p>
+              <p class="hours-Aax">Hours</p>
+            </div>
+            <div class="group-20-SYU">
+              <p class="item-48-bRN">48</p>
+              <p class="min-hzC">Min</p>
+            </div>
+          </div>
+          <div class="group-19-a2Q">BUY NOW</div>
+        </div>
+        <div class="group-17i">
+          <div class="auto-group-bbor-iH2">
+            <div class="ellipse-3-FGx">
+            </div>
+            <div class="ellipse-8-BwJ">
+            </div>
+            <div class="ellipse-13-Y1A">
+            </div>
+            <div class="ellipse-18-VBJ">
+            </div>
+          </div>
+          <div class="auto-group-ztba-2wv">
+            <div class="ellipse-4-kd2">
+            </div>
+            <div class="ellipse-9-6B6">
+            </div>
+            <div class="ellipse-14-3c8">
+            </div>
+            <div class="ellipse-19-Pvt">
+            </div>
+          </div>
+          <div class="auto-group-2xl8-iiG">
+            <div class="ellipse-5-4GL">
+            </div>
+            <div class="ellipse-10-1Ba">
+            </div>
+            <div class="ellipse-15-9Yg">
+            </div>
+            <div class="ellipse-20-6ip">
+            </div>
+          </div>
+          <div class="auto-group-sqpv-eVS">
+            <div class="ellipse-6-nrY">
+            </div>
+            <div class="ellipse-11-Lt4">
+            </div>
+            <div class="ellipse-16-gwv">
+            </div>
+            <div class="ellipse-21-qK2">
+            </div>
+          </div>
+          <div class="auto-group-b61r-Bdn">
+            <div class="ellipse-7-LFW">
+            </div>
+            <div class="ellipse-12-A6q">
+            </div>
+            <div class="ellipse-17-GX7">
+            </div>
+            <div class="ellipse-22-Q8Y">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
+// php code for frame 3
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8" />
+  <link rel="icon" href="/favicon.ico" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="theme-color" content="#000000" />
+  <title>F</title>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poly%3A400"/>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro%3A400"/>
+  <link rel="stylesheet" href="./styles/f.css"/>
+</head>
+<body>
+<div class="f-7M6">
+  <div class="frame-2-qH6">
+    <div class="auto-group-1yue-MWL">
+      <div class="auto-group-d8gl-tWG">ABOUT US</div>
+      <img class="zalika-website-banner-jewelry-1-M8x" src="./assets/zalika-website-banner-jewelry-1.png"/>
+    </div>
+    <div class="auto-group-yfgg-xPe">
+      <div class="frame-3-7GY">
+      </div>
+      <div class="line-1-qyE">
+      </div>
+      <div class="frame-10-mbz">
+        <div class="welcome-to-mahwishs-jewelery-store-we-proudly-showcases-its-exciting-new-range-of-stylish-925-silver-jewellery-including-our-production-collection-and-silver-band-ring-collection-all-widths-all-sizes-our-designs-sell-well-helping-you-get-the-quickest-return-on-your-initial-wholesale-investment-our-range-is-made-from-the-finest-925-solid-silver-and-complimented-with-a-variety-of-stones-we-also-offer-a-beautiful-range-of-high-quality-designer-silver-jewellery-which-makes-them-stand-apart-from-the-competition-all-orders-are-shipped-directly-from-thailand-with-care-and-true-customer-satisfactioninmind-WJg">
+          <?php
+          echo '<span class="welcome-to-mahwishs-jewelery-store-we-proudly-showcases-its-exciting-new-range-of-stylish-925-silver-jewellery-including-our-production-collection-and-silver-band-ring-collection-all-widths-all-sizes-our-designs-sell-well-helping-you-get-the-quickest-return-on-your-initial-wholesale-investment-our-range-is-made-from-the-finest-925-solid-silver-and-complimented-with-a-variety-of-stones-we-also-offer-a-beautiful-range-of-high-quality-designer-silver-jewellery-which-makes-them-stand-apart-from-the-competition-all-orders-are-shipped-directly-from-thailand-with-care-and-true-customer-satisfactioninmind-WJg-sub-0"> </span>';
+          echo '<span class="welcome-to-mahwishs-jewelery-store-we-proudly-showcases-its-exciting-new-range-of-stylish-925-silver-jewellery-including-our-production-collection-and-silver-band-ring-collection-all-widths-all-sizes-our-designs-sell-well-helping-you-get-the-quickest-return-on-your-initial-wholesale-investment-our-range-is-made-from-the-finest-925-solid-silver-and-complimented-with-a-variety-of-stones-we-also-offer-a-beautiful-range-of-high-quality-designer-silver-jewellery-which-makes-them-stand-apart-from-the-competition-all-orders-are-shipped-directly-from-thailand-with-care-and-true-customer-satisfactioninmind-WJg-sub-1">';
+          echo 'WELCOME to Mahwish’s jewelery store , we  proudly showcases its exciting new range of stylish 925 silver jewellery, including our Production Collection and Silver band Ring Collection , all widths all sizes';
+          echo '<br/>';
+          echo 'Our designs sell well helping you get the  quickest return on your initial wholesale investment.';
+          echo '<br/>';
+          echo 'Our range is made from the finest 925 solid silver and complimented with a variety of stones.';
+          echo '<br/>';
+          echo 'We also offer a beautiful range of high quality designer silver jewellery which makes them stand apart from the competition';
+          echo '<br/>';
+          echo 'All orders are shipped directly from Thailand with care and true customer satisfaction in mind.';
+          echo '</span>';
+          ?>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
